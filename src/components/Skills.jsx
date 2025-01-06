75a8303e26f1d93ac756ea78ce1530a18d17e55d
priyans619 <priyansh.2b@gmail.com>
2025-01-06 18:20:23 +0530

    add package

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -21,40 +21,41 @@ function Skills() {
           <p className="py-4">These are the technologies I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 ">
-          <div className="pt-4 rounded-md bg-slate-700 hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="pt-4 rounded-md bg-black border border-white hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
             <img src={HTML} alt="HTML icon" className="w-20 mx-auto drop-shadow-lg" />
-            <p className="my-4 font-semibold">Html</p>
+            <p className="my-4 font-semibold text-white">Html</p>
           </div>
-          <div className="pt-4 rounded-md bg-slate-700 hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
+          <div className="pt-4 rounded-md bg-black border border-white hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
             <img src={CSS} alt="CSS icon" className="w-20 mx-auto drop-shadow-lg" />
-            <p className="my-4 font-semibold">Css</p>
+            <p className="my-4 font-semibold text-white">Css</p>
           </div>
-          <div className="pt-4 rounded-md bg-slate-700 hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
+          <div className="pt-4 rounded-md bg-black border border-white hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
             <img src={Javascript} alt="Javascript icon" className="w-20 mx-auto drop-shadow-lg" />
-            <p className="my-4 font-semibold">Javascript</p>
+            <p className="my-4 font-semibold text-white">Javascript</p>
           </div>
-          <div className="pt-4 rounded-md bg-slate-700 hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
+          <div className="pt-4 rounded-md bg-black border border-white hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
             <img src={ReactImg} alt="React icon" className="w-20 mx-auto drop-shadow-lg" />
-            <p className="my-4 font-semibold">React</p>
+            <p className="my-4 font-semibold text-white">React</p>
           </div>
-          <div className="pt-4 rounded-md bg-slate-700 hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
+          <div className="pt-4 rounded-md bg-black border border-white hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
             <img src={GitHub} alt="Github icon" className="w-20 mx-auto drop-shadow-lg" />
-            <p className="my-4 font-semibold">GitHub</p>
+            <p className="my-4 font-semibold text-white">GitHub</p>
           </div>
-          <div className="pt-4 rounded-md bg-slate-700 hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
+          <div className="pt-4 rounded-md bg-black border border-white hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
             <img src={Node} alt="Node icon" className="w-20 mx-auto drop-shadow-lg" />
-            <p className="my-4 font-semibold">Node Js</p>
+            <p className="my-4 font-semibold text-white">Node Js</p>
           </div>
-          <div className="pt-4 rounded-md bg-slate-700 hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
+          <div className="pt-4 rounded-md bg-black border border-white hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
             <img src={Mongo} alt="Firebase icon" className="w-20 mx-auto drop-shadow-lg" />
-            <p className="my-4 font-semibold">Mongo DB</p>
+            <p className="my-4 font-semibold text-white">Mongo DB</p>
           </div>
-          <div className="pt-4 rounded-md bg-slate-700 hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
+          <div className="pt-4 rounded-md bg-black border border-white hover:bg-[#1a1a1a] hover:shadow-md hover:shadow-[#6BD425] hover:scale-105 duration-700 hover:duration-700 transition-all hover:transition-all">
             <img src={Tailwind} alt="Tailwind icon" className="w-20 mx-auto drop-shadow-lg" />
-            <p className="my-4 font-semibold">Tailwind</p>
+            <p className="my-4 font-semibold text-white">Tailwind</p>
           </div>
         </div>
+
       </div>
     </div>
   );
